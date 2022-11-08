71b226f1170bdc14ed90815625e77472137eded8
Adeline <k4adel2020@gmail.com>
2022-11-08 20:09:17 +0600

    second repo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Laptop myLaptop = new Laptop("Asus", 16, "14.07.2021");
         System.out.println(myLaptop);
+        System.out.println("новая ветка");
     }
 }
